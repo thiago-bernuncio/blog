1ba2c520211a30ddefeeafe64371c889932e0e0c
Thiago Bernuncio <thiago.bernuncio@gmail.com>
2024-03-30 20:22:52 -0300

    Criação e estilização da págia de criação de post

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import './NewPost.css'
 
 const NewPost = () => {
     return (
-        <div>
-            NewPost
+        <div className='new-post'>
+            <form>
+                <div className='form-control'>
+                    <label htmlFor="title">Título:</label>
+                    <input type="text" name="title" id='title' placeholder="Digite o título do seu post" />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="body">Conteúdo:</label>
+                    <textarea name="body" id="body" placeholder='Digite o conteúdo'></textarea>
+                </div>
+                <input type="submit" value="Criar Post" className='btn' />
+            </form>
         </div>
     )
 }
