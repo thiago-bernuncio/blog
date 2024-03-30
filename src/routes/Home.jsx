e7969fdf14efe9ded741ce4527a198117ea2a5d4
Thiago Bernuncio <thiago.bernuncio@gmail.com>
2024-03-30 19:12:37 -0300

    Criado a função para resgate de dados da API com axios

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+// axios
+import axios from 'axios'
+
+// CSS
+import './Home.css'
 
 const Home = () => {
+
+    const [posts, setPosts] = useState([])
+
+    const getPosts = async () => {
+        try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+            const data = response.data
+
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getPosts()
+    }, [])
+
+
     return (
         <div>Home</div>
     )
