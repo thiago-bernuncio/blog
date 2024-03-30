37426d1d7cf24a4b51cd44d16972718f0522b239
Thiago Bernuncio <thiago.bernuncio@gmail.com>
2024-03-30 17:56:56 -0300

    Adicionando a configuração de rotas, React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { Outlet } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <div className='App'>
-      <h1>React Axios</h1>
+      <div className='container'>
+        <Outlet />
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom'
+
+
+// páginas
+import Home from './routes/Home.jsx'
+import NewPost from './routes/NewPost.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
