5dab7e41bdff80b7c81a1a1b608194d31f2680de
Thiago Bernuncio <thiago.bernuncio@gmail.com>
2024-03-30 19:25:15 -0300

    Adicionando os posts na Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -17,7 +17,8 @@ const Home = () => {
 
             const data = response.data
 
-            console.log(data)
+            setPosts(data)
+
         } catch (error) {
             console.log(error)
         }
@@ -29,7 +30,18 @@ const Home = () => {
 
 
     return (
-        <div>Home</div>
+        <div>
+            <h1>Últimos posts</h1>
+            {posts.length === 0 ? <p>Carregando...</p> : (
+                posts.map((post) => (
+                    <div className='post' key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                        <Link to={`/posts/${post.id}`} className='btn'>Ler mais</Link>
+                    </div>
+                ))
+            )}
+        </div>
     )
 }
 
